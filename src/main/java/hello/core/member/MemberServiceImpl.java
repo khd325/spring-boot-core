bde5a5e6bcfac8afa9498347f7253d993f986168
hdk325 <hdk325@naver.com>
2022-03-02 16:31:56 +0900

    3.싱글톤 컨테이너

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -13,8 +13,16 @@ public class MemberServiceImpl implements MemberService{
         memberRepository.save(member);
     }
 
+
     @Override
     public Member findMember(Long memberId) {
+
+
         return memberRepository.findById(memberId);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
